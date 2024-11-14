4cf652309d99fb0ca236943fb5f7e2c46e861efc
Alithebest-coder <aurumprint@proton.me>
2024-11-14 17:41:59 +1000

    Add files via upload

---
+++ and_or.py
@@ -0,0 +1,17 @@
+a=10
+b=43
+c=0
+
+
+if a and b and c:
+    print("exists")
+    
+else:
+    print("not exists")
+    
+    
+if a or b:
+    print("a or c exists")
+    
+else: 
+    print("do not exists")
---
+++ not.py
@@ -0,0 +1,11 @@
+a=15
+b=18
+c=15
+print(a!=b)
+print(b==c)
+print(a==c)
+print(a!=c)
+print(b>a)
+print(a>c)
+print(a<b and b>c)
+print(not False)
